5e4f8a9a7830520d6d5dd80c28dd96948c1bc177
Amish Bharti <47895005+amish1999@users.noreply.github.com>
2022-09-30 23:35:33 +0530

    Update meeting.js

--- js/meeting.js
+++ js/meeting.js
@@ -42,56 +42,8 @@ function websdkready() {
     china: tmpArgs.china === "1",
   };
 
-  // a tool use debug mobile device
-  if (testTool.isMobileDevice()) {
-    vConsole = new VConsole();
-  }
-  console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
-
-  // it's option if you want to change the WebSDK dependency link resources. setZoomJSLib must be run at first
-  // ZoomMtg.setZoomJSLib("https://source.zoom.us/1.9.6/lib", "/av"); // CDN version defaul
-  if (meetingConfig.china)
-    ZoomMtg.setZoomJSLib("https://jssdk.zoomus.cn/1.9.6/lib", "/av"); // china cdn option
-  ZoomMtg.preLoadWasm();
-  ZoomMtg.prepareJssdk();
-  function beginJoin(signature) {
-    ZoomMtg.init({
-      leaveUrl: meetingConfig.leaveUrl,
-      webEndpoint: meetingConfig.webEndpoint,
-      disableCORP: !window.crossOriginIsolated, // default true
-      // disablePreview: false, // default false
-      success: function () {
-        console.log(meetingConfig);
-        console.log("signature", signature);
-        ZoomMtg.i18n.load(meetingConfig.lang);
-        ZoomMtg.i18n.reload(meetingConfig.lang);
-        ZoomMtg.join({
-          meetingNumber: meetingConfig.meetingNumber,
-          userName: meetingConfig.userName,
-          signature: signature,
-          apiKey: meetingConfig.apiKey,
-          userEmail: meetingConfig.userEmail,
-          passWord: meetingConfig.passWord,
-          success: function (res) {
-            console.log("join meeting success");
-            console.log("get attendeelist");
-            ZoomMtg.getAttendeeslist({});
-            ZoomMtg.getCurrentUser({
-              success: function (res) {
-                console.log("success getCurrentUser", res.result.currentUser);
-              },
-            });
-          },
-          error: function (res) {
-            console.log(res);
-          },
-        });
-      },
-      error: function (res) {
-        console.log(res);
-      },
-    });
-
+// / / 
+  
     ZoomMtg.inMeetingServiceListener('onUserJoin', function (data) {
       console.log('inMeetingServiceListener onUserJoin', data);
     });
